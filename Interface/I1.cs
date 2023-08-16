5012c8785aae4efc9054ed653ac92b90e00b6a39
girishjondhale <girishjondhale1997@gmail.com>
2023-08-16 16:36:41 -0700

    Add project files.

---
+++ Interface/I1.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Security.Cryptography.X509Certificates;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Interface
+//{ 
+//    public interface Interface1
+//    {
+//        string ToString();
+//    }
+
+//    public class Square : Interface1
+//    {
+//        int side;
+//        int area, parimeter;
+
+//        public Square(int side)
+//        {
+//            this.side = side;
+
+//        }
+//        public void Calculate()
+//        {
+//            area = side * side;
+//            //perimeter =4* side;
+//        }
+//        public string ToString()
+//        {
+//            return $"area of square={area}";
+//        }
+
+//    }
+//}
---
+++ Interface/Program.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Interface
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Square s1=new Square(6);
+//            s1.Calculate();
+//            Console.WriteLine(s1.ToString());
+//        }
+//    }
+//}
